c34e6a448227b90125204505e3f5df9b78c67f40
Gonzaloav <Gonzaloav@protonmail.com>
2022-03-10 17:12:38 +0100

    caso1

--- Case1/js/controllers/main.mjs
+++ Case1/js/controllers/main.mjs
@@ -2,9 +2,13 @@ export function dataToHTMLList (array) {
     const HTMLElements = array.map(
         (item) => {
             const li = document.createElement("li");
-            li.innerText = `Importe: ${item} puntos.`;
+            li.innerText = `Importe: ${item/2} puntos.`;
             return li;
         }
     )
     document.querySelector("#list").append(...HTMLElements);
 }
+/**Modifica el código de este sitio para dividir entre dos los valores
+de los modelos antes de mostrarlos. Solucionalo sin escribir nuevas funciones.
+ * En la linea 5 cambiar ${item} por ${item/2}*/
+ 
